9b215ce8e328afd4684edc52080417cba7989910
faizshaik16 <faizshaik16@gmail.com>
2015-04-27 22:33:28 -0400

    web service project

---
+++ FaizSoapProducer/src/com/faiz/soap/producer/FaizProducerService.java
@@ -0,0 +1,13 @@
+package com.faiz.soap.producer;
+
+public class FaizProducerService {
+	
+	public int add(int a, int b){
+		return a+b;
+	}
+	
+	public int multiply(int a, int b){
+		return a*b;
+	}
+
+}
